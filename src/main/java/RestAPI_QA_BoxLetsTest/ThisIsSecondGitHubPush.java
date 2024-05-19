df6f93df3578452a3957a33893e85f09ecb02d6d
Kishan Kumar Pandey <Kishanpandey6272@gmail.com>
2024-05-19 10:23:01 +0530

    this is commit for the practise

---
+++ src/main/java/RestAPI_QA_BoxLetsTest/ThisIsSecondGitHubPush.java
@@ -0,0 +1,13 @@
+package RestAPI_QA_BoxLetsTest;
+
+import org.testng.annotations.Test;
+
+public class ThisIsSecondGitHubPush {
+
+    @Test
+    public void test01()
+    {
+        int a=10;
+        System.out.println(a);
+    }
+}
